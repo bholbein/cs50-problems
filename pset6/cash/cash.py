3883421979533d7f75a9b1d6323c6a2e744e0d92
Bernd Holbein <bernd.holbein@googlemail.com>
2020-10-13 21:36:59 +0200

    reduced number of variables

--- pset6/cash/cash.py
+++ pset6/cash/cash.py
@@ -5,10 +5,8 @@ coins = [0.25, 0.1, 0.05, 0.01]
 def main():
     print(round(calc_change(get_input())))
 
-
 def get_input():
-    """Return positive float number as valid user input"""
-    cash = 0.0
+    """Return positive float number as valid user input"""    
     while True:
         try:
             cash = float(input("Change owned: "))
@@ -24,13 +22,11 @@ def get_input():
 def calc_change(cash: float):
     """Returns minimal number of coins for change"""
     counter = 0
-    rest = cash
     for i in coins:
-        counter = counter + (rest // i)
-        rest = round(rest % i, 2)
-        if rest == 0:
+        counter = counter + (cash // i)
+        cash = round(cash % i, 2)
+        if cash == 0:
             break
     return counter
-
 
 main()
