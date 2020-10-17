134cc8cc79f3eb9d4097a3ff72ba78117c42e18e
Bernd Holbein <bernd.holbein@googlemail.com>
2020-10-17 21:15:04 +0200

    reordered functions in pset6/dna

--- pset6/dna/dna.py
+++ pset6/dna/dna.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
 # dependencies
-import sys
 import csv
 import re
+import sys
 
 
 def main(argv):
@@ -14,6 +14,49 @@ def main(argv):
         print(cal_match(data, check_sequence(data, sequence[0])))
     else:
         print("Usage: python dna.py data.csv sequence.txt")
+
+
+def cal_match(data: list, sequence_count: dict) -> str:
+    ### match data with sequence count ###
+
+    for list_item in data:
+        match_counter = 0
+        for k, v in list_item.items():
+            #print(f"value: {k}:{v}, sequence: {k}:{sequence_count[k]}")
+            if k == 'name':
+                pass
+            elif int(v) == sequence_count[k]:
+                match_counter += 1
+        if match_counter == len(sequence_count) - 1:
+            return list_item['name']
+    return "No match"
+
+
+def check_sequence(data: list, sequence: str) -> dict:
+    ### check the dna sequence based on STRs ###
+    keys = set()
+    count = {}
+    for entry in data:
+        for k in entry.keys():
+            keys.add(k)
+    for item in keys:
+        # define several helper variable for search algorithm
+        start = counter = iteration = max_counter = 0
+        sequence_temp = sequence
+        while item in sequence_temp:
+            start = sequence_temp.index(item)
+            if (start == 0) or (iteration == 0):
+                counter += 1
+                # check if squence is longest until this point
+                if counter > max_counter:
+                    max_counter = counter
+            else:
+                # reset counter
+                counter = 1
+            sequence_temp = sequence_temp[start + len(item):]
+            iteration += 1
+        count[item] = max_counter
+    return count
 
 
 def get_input(argv) -> dict:
@@ -41,49 +84,6 @@ def read_txt(input_file: str) -> list:
     with open(input_file, mode='r') as txtfile:
         for row in csv.reader(txtfile):
             return row
-
-
-def check_sequence(data: list, sequence: str) -> dict:
-    ### check the dna sequence based on STRs ###
-    keys = set()
-    count = {}
-    for entry in data:
-        for k in entry.keys():
-            keys.add(k)
-    for item in keys:
-        #define several helper variable for search algorithm
-        start = counter = iteration = max_counter = 0
-        sequence_temp = sequence
-        while item in sequence_temp:
-            start = sequence_temp.index(item)
-            if (start == 0) or (iteration == 0):
-                counter += 1
-                #check if squence is longest until this point
-                if counter > max_counter:
-                    max_counter = counter
-            else:
-                #reset counter
-                counter = 1
-            sequence_temp = sequence_temp[start + len(item):]
-            iteration += 1
-        count[item] = max_counter
-    return count
-
-
-def cal_match(data: list, sequence_count: dict) -> str:
-    ### match data with sequence count ###
-
-    for list_item in data:
-        match_counter = 0
-        for k, v in list_item.items():
-            #print(f"value: {k}:{v}, sequence: {k}:{sequence_count[k]}")
-            if k == 'name':
-                pass
-            elif int(v) == sequence_count[k]:
-                match_counter += 1
-        if match_counter == len(sequence_count) - 1:
-            return list_item['name']
-    return "No match"
 
 
 if __name__ == "__main__":
